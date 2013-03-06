5c046528e991d443b1bd5dcb0241359ee22c47a1
medimatt <awesomenacho@ymail.com>
2013-03-06 11:45:48 -0500

    prevents tertiarySector from being > population

--- public/game.js
+++ public/game.js
@@ -249,13 +249,16 @@ city.primarySector = 0;
 	for (primary in city.blocks)
 	city.primarySector += 100*Math.pow(1.17,(city.blocks[primary].level - 1));
 city.secondarySector += hp*(Math.log((city.fuel+(city.secondarySector/2))/city.secondarySector)*city.secondarySector)*((city.buildings[3]+(city.buildings[5]*2)+(city.buildings[7]*2)+(city.buildings[8]*3)+(city.buildings[10]*2))/10);
-city.tertiarySector += hp*(Math.log((city.secondarySector+1)/city.tertiarySector)*city.tertiarySector)*(((city.buildings[0]*3)+city.buildings[3]+(city.buildings[6]*2)+(city.buildings[9]*2)+city.buildings[11])+city.buildings[16]/10); //we'll make it a bit more complex later
+if (city.tertiarySector < city.secondarySector)
+	city.tertiarySector += hp*(Math.log((city.secondarySector+1)/city.tertiarySector)*city.tertiarySector)*(((city.buildings[0]*3)+city.buildings[3]+(city.buildings[6]*2)+(city.buildings[9]*2)+city.buildings[11])+city.buildings[16]/10); //we'll make it a bit more complex later
 productionTech = 10*(city.tertiarySector/city.secondarySector) + 0.1; //eventually we'll make this a part of research
 
-if ((city.primarySector+city.secondarySector) > city.population)
-	city.secondarySector = city.population-city.primarySector;
-else if ((city.primarySector+city.secondarySector+city.tertiarySector) > city.population)
+if ((city.primarySector+city.secondarySector+city.tertiarySector) > city.population)
 	city.tertiarySector = city.population-(city.primarySector+city.secondarySector);
+if (city.tertiarySector < 0) {
+	city.secondarySector += city.tertiarySector;
+	city.tertiarySector = 0;
+}
 	
 production = city.secondarySector*productionTech;
 
